5513d6e0fa9207590b2d748dc3bf131dafb3902a
Ajinkya X <ajinkyab1987@gmail.com>
2014-11-22 23:02:33 +0530

    minor fixes

--- js/init.js
+++ js/init.js
@@ -1,7 +1,6 @@
 /*
-	Strata by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+	Ajinkya Borade
+	www.ajinkyaxjs.com
 */
 
 (function($) {
